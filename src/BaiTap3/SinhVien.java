b058c77a05429fdbe706f61186ad465b37b74557
chaucamly <ly.cc.59cntt@ntu.edu.vn>
2020-04-25 09:39:55 +0700

    v1.1

---
+++ src/BaiTap3/SinhVien.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BaiTap3;
+import java.util.Date;
+
+/**
+ *
+ * @author Cam Ly
+ */
+public class SinhVien 
+{
+    private String hoTen;
+    private Date ngaySinh;
+    private float diemTB;
+    public SinhVien(String hoTen, Date ngaySinh, float diemTB)
+    {
+        this.hoTen=hoTen;
+        this.ngaySinh=ngaySinh;
+        this.diemTB=diemTB;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setDiemTB(float diemTB) {
+        this.diemTB = diemTB;
+    }
+
+    public float getDiemTB() {
+        return diemTB;
+    }
+   
+}
